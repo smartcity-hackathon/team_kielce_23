05e66dbd967f1bf4159cc021368edc5caf0eaaa4
mprzenzak <mprzenzak@gmail.com>
2018-06-15 15:33:27 +0200

    Buttons in main added

--- app/src/main/java/mprzenzak/com/drivewith/MainActivity.java
+++ app/src/main/java/mprzenzak/com/drivewith/MainActivity.java
@@ -1,7 +1,10 @@
 package mprzenzak.com.drivewith;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginActivityBtn = (Button) findViewById(R.id.loginActivityBtn);
+        loginActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
