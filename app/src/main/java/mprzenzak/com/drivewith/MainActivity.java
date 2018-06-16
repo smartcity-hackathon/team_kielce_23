716be847995f2799966ed95228ae77893fc22a31
mprzenzak <mprzenzak@gmail.com>
2018-06-16 03:32:13 +0200

    test

--- app/src/main/java/mprzenzak/com/drivewith/MainActivity.java
+++ app/src/main/java/mprzenzak/com/drivewith/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //dupa
 
         Button registerActivityBtn = (Button) findViewById(R.id.registerActivityBtn);
         registerActivityBtn.setOnClickListener(new View.OnClickListener() {
