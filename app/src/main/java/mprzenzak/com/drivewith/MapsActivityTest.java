b1c33eaf8bfb8e56a767c4011e324f9efdd6c041
mprzenzak <mprzenzak@gmail.com>
2018-06-16 07:16:12 +0200

    Search in map

--- app/src/main/java/mprzenzak/com/drivewith/MapsActivityTest.java
+++ app/src/main/java/mprzenzak/com/drivewith/MapsActivityTest.java
@@ -1,7 +1,11 @@
 package mprzenzak.com.drivewith;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +13,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivityTest extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,7 +30,27 @@ public class MapsActivityTest extends FragmentActivity implements OnMapReadyCall
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    public void onSearch(View view) {
+        EditText location_tf = (EditText) findViewById(R.id.findAddress);
+        String location = location_tf.getText().toString();
+        List<Address> addressList = null;
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
 
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -42,5 +69,6 @@ public class MapsActivityTest extends FragmentActivity implements OnMapReadyCall
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMinZoomPreference(13);
     }
 }
--- app/src/main/java/mprzenzak/com/drivewith/StartWindow.java
+++ app/src/main/java/mprzenzak/com/drivewith/StartWindow.java
@@ -43,15 +43,15 @@ public class StartWindow extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        Button openMapBtn = (Button) findViewById(R.id.openMapBtn);
-        openMapBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent openMapIntent = new Intent(getApplicationContext(), MapsActivityTest.class);
-                startActivity(openMapIntent);
-            }
-        });
+//        Button openMapBtn = (Button) findViewById(R.id.openMapBtn);
+//        openMapBtn.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                Intent openMapIntent = new Intent(getApplicationContext(), MapsActivityTest.class);
+//                startActivity(openMapIntent);
+//            }
+//        });
     }
 
     @Override
